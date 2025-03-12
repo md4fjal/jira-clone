a6459af30b3f987c5b6f37e1df7285ba6898594f
MD AFJAL <104983871+md4fjal@users.noreply.github.com>
2025-03-12 11:25:58 +0530

    componetns are modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Button variant={"destructive"}>Click Me</Button>
+      <Input />
+      <Button size={"lg"}>Click Me</Button>
+      <Button variant={"destructive"} size={"lg"}>
+        Click Me
+      </Button>
+      <Button variant={"secondary"} size={"lg"}>
+        Click Me
+      </Button>
+      <Button variant={"ghost"} size={"lg"}>
+        Click Me
+      </Button>
+      <Button variant={"muted"} size={"lg"}>
+        Click Me
+      </Button>
+      <Button variant={"outline"} size={"lg"}>
+        Click Me
+      </Button>
+      <Button variant={"teritary"} size={"lg"}>
+        Click Me
+      </Button>
       <p className="text-lime-500 text-center">hi, how are you doing?</p>
     </div>
   );
